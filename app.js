0f174b35575220ba2608508de8635d93fb2e2948
Nikhil-CH001 <nikhilchy001@gmail.com>
2025-06-04 19:08:02 +0545

    push

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
+const sequelize = require("./database/config")
 const express = require("express")
 const app = express()
 
+app.use(express.json());
+
 app.set("view engine", "ejs")
 
-app.get("/", (req,res)=>{
-    res.render("home")
+app.get("/",(req,res)=>{
+    res.render("home",{title: "ejs", page: "home"})
+})
+app.get("/about",(req,res)=>{
+    res.render("about",{title: "ejs", page: "about"})
+})
+app.get("/services",(req,res)=>{
+    res.render("services",{title: "ejs", page: "services"})
+})
+app.get("/contact",(req,res)=>{
+    res.render("contact",{title: "ejs", page: "contact"})
 })
 
 app.listen(4444, ()=>{
